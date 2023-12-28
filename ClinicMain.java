a6203919621a12e6b9d4b35ae02f1a77a64f4812
yamanj <yaman.jabi@gmail.com>
2023-12-28 00:14:57 -0500

    reset main commit

--- ClinicMain.java
+++ ClinicMain.java
@@ -7,25 +7,6 @@ class ClinicMain {
 public static void main(String[] args) {
 	
 	//search for patient if not found add new patient 
-		
-	 String[] items = {"One", "Two"};
-     JComboBox<String> combo = new JComboBox<String>(items);
-     JTextField field1 = new JTextField("1234.56");
-     JTextField field2 = new JTextField("9876.54");
-     JPanel panel = new JPanel();
-     panel.add(combo);
-    panel.add(new JLabel("Field 1:"));
-    panel.add(field1);
-    panel.add(new JLabel("Field 2:"));
-    panel.add(field2);
-   int result = JOptionPane.showConfirmDialog(null, panel, "Test",
-        JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
-    if (result == JOptionPane.OK_OPTION) {
-        System.out.println(combo.getSelectedItem()
-            + " " + field1.getText()
-            + " " + field2.getText());
-    } else {
-        System.out.println("Cancelled");
-    }
+	
 }
 }
